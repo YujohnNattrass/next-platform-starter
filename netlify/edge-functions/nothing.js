32e356d248a83ce349e8a34d4c1dcd5f519f8abc
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-15 12:57:47 -0800

    update func

--- netlify/edge-functions/nothing.js
+++ netlify/edge-functions/nothing.js
@@ -1,6 +1,16 @@
 export default async function nothing(request, context) {
-    console.log(`req made to ${request.url}`);
-    return await context.next();
+    console.log(`req made to ${request.url} has header: ${!!req.headers.get(`x-nothing`)}`);
+    if (req.headers.get(`x-nothing`)) {
+        return await context.next();
+    }
+
+    const headers = new Headers(Array.from(req.headers.entries()));
+    headers.set('x-nothing', '1');
+    const newReq = new Request(req, {
+        headers
+    });
+
+    return await fetch(newReq);
 }
 
 export const config = {
