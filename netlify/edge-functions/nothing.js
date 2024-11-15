6e38df39976876f361332bcc9080a750ebb8ccd6
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-15 13:17:27 -0800

    update request

--- netlify/edge-functions/nothing.js
+++ netlify/edge-functions/nothing.js
@@ -1,16 +1,26 @@
+import { HTMLRewriter } from 'https://raw.githubusercontent.com/worker-tools/html-rewriter/master/index.ts';
 export default async function nothing(req, context) {
-    console.log(`req made to ${req.url} has header: ${!!req.headers.get(`x-nothing`)}`);
-    if (req.headers.get(`x-nothing`)) {
-        return await context.next();
-    }
+    // console.log(`req made to ${req.url} has header: ${!!req.headers.get(`x-nothing`)}`);
+    // if (req.headers.get(`x-nothing`)) {
+    //     return await context.next();
+    // }
 
-    const headers = new Headers(Array.from(req.headers.entries()));
-    headers.set('x-nothing', '1');
-    const newReq = new Request(req, {
-        headers
-    });
+    // const headers = new Headers(Array.from(req.headers.entries()));
+    // headers.set('x-nothing', '1');
+    // const newReq = new Request(req, {
+    //     headers
+    // });
 
-    return await fetch(newReq);
+    // return await fetch(newReq);
+
+    const resp = context.next(req);
+    return new HTMLRewriter()
+        .on(`script,div`, {
+            element(element) {
+                element.setAttribute(`abc`, 1);
+            }
+        })
+        .transform(resp);
 }
 
 export const config = {
