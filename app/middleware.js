5840e4bbd0a0e2ab0a7b6fd0ae2390a5f48a4d2d
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-14 19:20:04 -0800

    fix

--- app/middleware.js
+++ app/middleware.js
@@ -3,5 +3,5 @@ import { MiddlewareRequest } from '@netlify/next';
 export async function middleware(nextRequest) {
     const request = new MiddlewareRequest(nextRequest);
     const resp = await request.next();
-    resp.headers.set('x-hello', 'world');
+    return resp.headers.set('x-hello', 'world');
 }
