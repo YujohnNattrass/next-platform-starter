5dbc37f2e9d681ee5af831b4226fdd18e3776169
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-18 15:00:27 -0800

    move middleware

--- middleware.js
+++ middleware.js
@@ -3,5 +3,6 @@ import { MiddlewareRequest } from '@netlify/next';
 export async function middleware(nextRequest) {
     const request = new MiddlewareRequest(nextRequest);
     const resp = await request.next();
+    console.log(`### RESP`, resp);
     return resp.headers.set('x-hello', 'world');
 }
