d4f15c518b14ba8fd34089eccf2cf02e36b3eac1
Yujohn Nattrass <yujohn.natt@gmail.com>
2024-11-18 10:34:43 -0800

    test new csp

---
+++ netlify/edge-functions/csp.js
@@ -0,0 +1,80 @@
+/* eslint-disable */
+import { csp } from 'https://deno.land/x/csp_nonce_html_transformer@v2.1.4/src/index.ts';
+// @ts-ignore
+
+const params = inputs;
+params.reportUri = params.reportUri || '/.netlify/functions/__csp-violations';
+// @ts-ignore
+params.distribution = Netlify.env.get('CSP_NONCE_DISTRIBUTION');
+
+params.strictDynamic = true;
+params.unsafeInline = true;
+params.self = true;
+params.https = true;
+params.http = true;
+
+const handler = async (request, context) => {
+    const response = await context.next(request);
+
+    // for debugging which routes use this edge function
+    response.headers.set('x-debug-csp-nonce', 'invoked');
+    return csp(response, params);
+};
+
+// Top 50 most common extensions (minus .html and .htm) according to Humio
+const excludedExtensions = [
+    'aspx',
+    'avif',
+    'babylon',
+    'bak',
+    'cgi',
+    'com',
+    'css',
+    'ds',
+    'env',
+    'gif',
+    'gz',
+    'ico',
+    'ini',
+    'jpeg',
+    'jpg',
+    'js',
+    'json',
+    'jsp',
+    'log',
+    'm4a',
+    'map',
+    'md',
+    'mjs',
+    'mp3',
+    'mp4',
+    'ogg',
+    'otf',
+    'pdf',
+    'php',
+    'png',
+    'rar',
+    'sh',
+    'sql',
+    'svg',
+    'ttf',
+    'txt',
+    'wasm',
+    'wav',
+    'webm',
+    'webmanifest',
+    'webp',
+    'woff',
+    'woff2',
+    'xml',
+    'xsd',
+    'yaml',
+    'yml',
+    'zip'
+];
+
+export const config = {
+    path: '/'
+};
+
+export default handler;
